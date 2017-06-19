e00bdaa65b54e66cc4febc60475bb0f38674551d
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-06-18 20:31:46 -0700

    Update and rename api/index.php to api/v1/index.php

---
+++ api/v1/index.php
@@ -0,0 +1,164 @@
+<?php
+
+if(
+  isset($_REQUEST['LongformURL'])&&
+  (!(trim($_REQUEST['LongformURL'])==''))
+){
+
+  $Longform=file_get_contents($_REQUEST['LongformURL']);
+  if($Longform==false){
+    echo '<p>Invalid URL</p>';
+  }else{
+    if(isset($_REQUEST['numberOfSentences'])){
+      $numberOfSentences = $_REQUEST['numberOfSentences'];
+    }else{
+      $numberOfSentences = 2;
+    }
+
+    //Clean up the contents of the page before condensing
+    $Longform = strip_tags($Longform);
+
+    echo Condense($Longform,$numberOfSentences);
+  }
+
+}elseif(isset($_REQUEST['longform'])){
+
+  echo Condense($_REQUEST['longform'],$_REQUEST['numberOfSentences']);
+
+}
+
+
+function Condense($Text,$NumberOfSentences = 1){
+  //Clean Up The Text
+  $CleanText = CleanUp($Text);
+
+  //Score Words
+  $Scores = GetWordScores($CleanText);
+
+  //Score Words
+  SortByScore($Scores, 'Score');
+
+  //Parse Sentences. Punctuation is irrelevant for the purposes of this algorithm.
+  $RawSentences = $Text;
+  $RawSentences = str_replace('?','.',$RawSentences);
+  $RawSentences = str_replace('!','.',$RawSentences);
+  $RawSentences = explode('.',$RawSentences);
+  $Sentences = array();
+  foreach($RawSentences as $RawSentence){
+    $CleanSentence = CleanUp($RawSentence);
+
+    $ThisSentenceScore = 0;
+
+    $SentenceScores = GetWordScores($CleanSentence);
+    foreach($SentenceScores as $SentenceScore){
+      $ThisSentenceScore += $SentenceScore['Score'] * FindScore($SentenceScore['Word'],$Scores);
+    }
+
+    $Sentences[] = array(
+      'Raw'   => $RawSentence,
+      'Clean' => $CleanSentence,
+      'Score' => $ThisSentenceScore
+    );
+  }
+
+  SortByScore($Sentences);
+
+  $Output='';
+  $NumberOfSentences-=1;
+  for($i = 0; $i <= $NumberOfSentences; $i++){
+   $Output = trim($Output).' '.$Sentences[$i]['Raw'];
+  }
+
+  return $Output;
+  //return $Sentences[0]['Raw'];
+}
+
+function CleanUp($Text){
+  $CleanText = strtolower($Text);
+  $CleanText = str_replace(PHP_EOL,".",$CleanText);
+  $CleanText = str_replace("'","",$CleanText);
+  $CleanText = str_replace('"','',$CleanText);
+  $CleanText = trim($CleanText);
+  return $CleanText;
+}
+
+function FindScore($Word,$Scores){
+  foreach($Scores as $Score){
+    if($Score['Word']==$Word){
+      return $Score['Score'];
+    }
+  }
+  return 0;
+}
+
+function GetWordScores($Text){
+  $WordScores = array_count_values(str_word_count($Text, 1));
+
+  $Scores  = array();
+  $Ignore=array('a','the','s','and','he','she','said','his','hers','with','in','is','of','that','have','not','on','to','be','it','like','only','was','from','more','many','so','who','also','would','an','at','doesn','t','i','for','think','be','function','var','com','if','in','has','been','or','are','you','this','as','these','your','my');
+
+  foreach($WordScores as $Word => $Score){
+    if(!(
+      in_array($Word,$Ignore)||
+      strlen($Word)<=1
+    )){
+      $Scores[] = array(
+        'Word'  => $Word,
+        'Score' => $Score
+      );
+    }
+  }
+  return $Scores;
+}
+
+function SortByScore(&$arr, $col = 'Score', $dir = SORT_DESC) {
+    $sort_col = array();
+    foreach ($arr as $key=> $row) {
+        $sort_col[$key] = $row[$col];
+    }
+
+    array_multisort($sort_col, $dir, $arr);
+}
+
+function ArrTabler($arr, $table_class = 'table tablesorter tablesorter-ice tablesorter-bootstrap', $table_id = null){
+  $return='';
+  if($table_id==null){
+    $table_id=md5(uniqid(true));
+  }
+  if(count($arr)>0){
+    $return.="\n<div class=\"table-responsive\">\n";
+    $return.= "\r\n".'	<table id="'.$table_id.'" class=" table'.$table_class.'">'."\n";
+    $first=true;
+    foreach($arr as $row){
+      if($first){
+        $return.= "		<thead>\n";
+        $return.= "			<tr>\n";
+        foreach($row as $key => $value){
+          $return.= "				<th>".ucwords($key)."</th>\n";
+        }
+        $return.= "			</tr>\n";
+        $return.= "		</thead>\n";
+        $return.= "		<tbody>\n";
+      }
+      $first=false;
+      $return.= "			<tr>\n";
+      foreach($row as $key => $value){
+        $return.="<td>".$value."</td>";
+      }
+      $return.= "			</tr>\n";
+    }
+    $return.= "		</tbody>\n";
+    $return.= "	</table>\n";
+    $return.= "</div>\n";
+    $return.= "<script>$('#".$table_id."').tablesorter({widgets: ['zebra', 'filter']});</script>\n";
+  }else{
+    $return.="No Results Found.";
+  }
+  return $return;
+}
+
+function pd($Var){
+  echo '<pre>';
+  var_dump($Var);
+  echo '</pre>';
+}
