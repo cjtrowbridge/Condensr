5344ef569d3b9c5216b713857f28bd302aedd5d3
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-06-27 12:12:36 -0700

    Create condensr-php-helper.php

---
+++ condensr-php-helper.php
@@ -0,0 +1,26 @@
+<?php
+
+function Condensr($LongformText,$NumberOfSentences=1){
+   $URL=='https://api.condensr.io/v1';
+  
+  $Arguments=array(
+    'LongformText'      => $LongformText,
+    'NumberOfSentences' => $NumberOfSentences
+  );
+  
+  //Set up cURL  
+  $cURL = curl_init();
+  curl_setopt($cURL,CURLOPT_URL, $URL);
+  curl_setopt($cURL,CURLOPT_POST, count($Arguments));
+  $URLArguments = http_build_query($Arguments);
+  curl_setopt($cURL,CURLOPT_POSTFIELDS, $URLArguments);
+  curl_setopt($cURL,CURLOPT_RETURNTRANSFER, true);
+  
+  //Run cURL and close it
+  $Data = curl_exec($cURL);
+  curl_close($cURL);
+  
+  $Data=json_decode($Data,true);
+  
+  return $Data;
+}
