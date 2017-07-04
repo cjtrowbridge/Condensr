00adc5f8af6579391e0d019d290f8b428f203839
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-07-04 15:27:30 -0700

    Update index.php

--- api/v1/index.php
+++ api/v1/index.php
@@ -1,9 +1,9 @@
 <?php 
 
 header('Access-Control-Allow-Origin: *');
-//ini_set('display_errors', 1);
-//ini_set('display_startup_errors', 1);
-//error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 
 if(
