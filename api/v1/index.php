934cd571487f98e89eba7b07f263eda4380bee3d
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-07-04 14:59:59 -0700

    Update index.php

--- api/v1/index.php
+++ api/v1/index.php
@@ -1,6 +1,10 @@
 <?php 
 
 header('Access-Control-Allow-Origin: *');
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 
 if(
   isset($_REQUEST['LongformURL'])&&
